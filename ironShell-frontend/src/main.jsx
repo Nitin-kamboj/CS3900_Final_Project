801a336be887e255d37e4c96bc3ccffcac41eeb1
Nitin-kamboj <nitinmirok201@gmail.com>
2026-03-25 19:13:54 -0400

    added tanstack router to route through __root not app.jsx

--- ironShell-frontend/src/main.jsx
+++ ironShell-frontend/src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
