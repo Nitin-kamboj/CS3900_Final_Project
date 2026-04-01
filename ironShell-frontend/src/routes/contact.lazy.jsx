71c18b850cc143e8f04c820d630ca6db2c864b0f
Nitin-kamboj <nitinmirok201@gmail.com>
2026-04-01 19:16:48 -0400

    Ready to deploy

--- ironShell-frontend/src/routes/contact.lazy.jsx
+++
@@ -1,6 +0,0 @@
-import { createLazyFileRoute } from '@tanstack/react-router'
-import { Contact } from '../components/contact.jsx'
-
-export const Route = createLazyFileRoute('/contact')({
-  component:() => <Contact/>,
-})
--- ironShell-frontend/src/routes/plans.lazy.jsx
+++
@@ -1,9 +0,0 @@
-import { Plans } from "../components/plans";
-import { createLazyFileRoute } from "@tanstack/react-router";
-
-export const Route = createLazyFileRoute("/plans")({
-  component: ()=> {
-  console.log("PlanPage loaded");
-  return <Plans />;
-}
-});
